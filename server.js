6d4c66c28f71acc92a1acdb00624cb25246a1679
Alyson Orta <alyson.orta@gmail.com>
2021-07-17 16:35:17 -0400

    updates

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //DEPENDENCIES
 const express = require('express');
-const path = require('path');
+
 
 //EXPRESS CONFIG
 const app = express();
@@ -11,9 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //ROUTER
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 
 //LISTENER
